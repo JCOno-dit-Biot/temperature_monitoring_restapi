ea6539f1730257e81dda8de17ff93a566c58f3e8
JCOno-dit-Biot <jc.onoditb@gmail.com>
2024-01-12 17:36:20 -0500

    implement basic model tests

---
+++ tests/test_models.py
@@ -0,0 +1,18 @@
+from src.models import *
+from datetime import datetime
+
+
+def test_create_room():
+    bedroom = room('bedroom')
+    assert bedroom.name == 'bedroom'
+
+def test_create_measurement():
+    measurement_time = datetime.utcnow()
+    data_entry = measurement(
+        timestamp = measurement_time,
+        temperature = 20.5,
+        humidity = .56
+    )
+
+    assert data_entry.timestamp == measurement_time and data_entry.temperature == 20.5 and data_entry.humidity == 0.56
+
