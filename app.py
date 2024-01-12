03b28940f0235cd4490d26dfd8f20ef90c6d2759
JCOno-dit-Biot <jc.onoditb@gmail.com>
2024-01-12 17:12:18 -0500

    implement flask app + connection to postgres

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+
+connection = psycopg2.connect(
+    host = "localhost",
+    database= os.getenv("POSTGRES_DB_NAME"),
+    user= os.getenv("POSTGRES_USER_NAME"),
+    password= os.getenv("POSTGRES_PWD"),
+    port = 5432
+)
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hello, world!"
+
