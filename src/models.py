d2ca7565afcbbf75c51d213099cc0a2313e87917
JCOno-dit-Biot <jc.onoditb@gmail.com>
2024-01-12 17:47:49 -0500

    measurement as dataclass

--- src/models.py
+++ src/models.py
@@ -6,8 +6,8 @@ class room():
     def __init__(self, name):
         self.name = name
 
-dataclass()
-class measurement():
+@dataclass
+class measurement:
     timestamp : datetime
     temperature: float
     humidity: float
