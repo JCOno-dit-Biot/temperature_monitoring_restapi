ce8a2b6f5c98b1d21f0a1fff63ade267d6fce096
JCOno-dit-Biot <jc.onoditb@gmail.com>
2024-02-17 12:44:08 -0500

    update table structure to accomodate two types of sensors

--- SQLscripts/createTables.sql
+++ SQLscripts/createTables.sql
@@ -1,17 +1,35 @@
-CREATE TABLE IF NOT EXISTS "rooms" (
+CREATE TABLE IF NOT EXISTS "room" (
     "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(100) UNIQUE  
+    "name" VARCHAR(100) NOT NULL UNIQUE  
 );
 
-CREATE TABLE IF NOT EXISTS "sensor_entry" (
+CREATE TABLE IF NOT EXISTS "sensor"(
+    "serial_number" INTEGER PRIMARY KEY,
+    "type" VARCHAR(100),
     "room_id" INTEGER,
-    "entry_timestamp" TIMESTAMPTZ PRIMARY KEY,
+    CONSTRAINT "sensor_entry_fkey_room_id_id" FOREIGN KEY ("room_id") REFERENCES "room"("id")
+);
+
+CREATE TABLE IF NOT EXISTS "humidity_temperature_entry" (
+    "sensor_serial" INTEGER,
+    "entry_timestamp" TIMESTAMPTZ,
     "temperature" FLOAT,
     "humidity" FLOAT,
-    CONSTRAINT "sensor_entry_fkey_roomid_id" FOREIGN KEY ("room_id") REFERENCES "rooms"("id")
+    CONSTRAINT "humidity_temperature_pkey_serial_timestamp" PRIMARY KEY ("sensor_serial", "entry_timestamp"),
+    CONSTRAINT "humidity_temperature_entry_fkey_sensor_sensor_serial" FOREIGN KEY ("sensor_serial") REFERENCES "sensor"("serial_number")
 );
 
-select * from "rooms";
+CREATE TABLE IF NOT EXISTS "wetness_entry" (
+    "sensor_serial" INTEGER,
+    "entry_timestamp" TIMESTAMPTZ,
+    "wetnesse" FLOAT,
+    CONSTRAINT "wetness_pkey_serial_timestamp" PRIMARY KEY ("sensor_serial", "entry_timestamp"),
+    CONSTRAINT "wetness_entry_fkey_sensor_sensor_serial" FOREIGN KEY ("sensor_serial") REFERENCES "sensor"("serial_number")
+);
 
-INSERT INTO rooms (name)
+
+
+-- select * from "rooms";
+
+INSERT INTO room (name)
     VALUES ('default room')
