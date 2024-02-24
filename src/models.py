6b266329460c56187611ff89d670ecd7ea60457a
JCOno-dit-Biot <jc.onoditb@gmail.com>
2024-02-24 16:12:15 -0500

    add unique constraint to plant and room

--- src/models.py
+++ src/models.py
@@ -2,24 +2,39 @@
 from datetime import datetime
 import logging
 
-from typing import Optional
+from typing import Optional, List
 from sqlmodel import SQLModel, Field, Relationship
 
 
 class Room(SQLModel, table = True):
     __tablename__ = "room"
     id : int = Field (default = None, primary_key=True)
-    name: str
-    sensor: Optional["Sensor"] = Relationship(back_populates="room")
+    name: str = Field(unique=True)
+    sensor: List[Optional["Sensor"]] = Relationship(back_populates="room")
+    plant: List[Optional["Plant"]] = Relationship(back_populates="room")
+
+class Plant(SQLModel, table=True):
+    __tablename__ = "plant"
+    id: int = Field (default = None, primary_key=True)
+    room_id: Optional[int] = Field(default=None, foreign_key="room.id")
+    room: Optional[Room] = Relationship(back_populates="plant")
+    name: str = Field(unique=True)
+    sensor: Optional["PlantSensor"] = Relationship(back_populates="plant")
 
 class Sensor(SQLModel, table = True):
     __tablename__ = "sensor"
     room_id: Optional[int] = Field(default=None, foreign_key="room.id")
     room: Optional[Room] = Relationship(back_populates="sensor")
     serial_number: int = Field(default = 0, primary_key=True)
-    type: str
     humidity_temperature_entry: Optional["HumityTemperatureEntry"]= Relationship(back_populates = "sensor")
-    wetness_entry: Optional["WetnessEntry"]= Relationship(back_populates = "sensor")
+
+class PlantSensor(SQLModel, table = True):
+    __tablename__ = "plant_sensor"
+    plant_id: Optional[int] = Field(default=None, foreign_key="plant.id")
+    plant: Optional[Plant] = Relationship(back_populates="sensor")
+    serial_number: int = Field(default = 0, primary_key=True)
+    plant_sensor_entry: Optional["PlantSensorEntry"]= Relationship(back_populates = "sensor")
+  
 
 class HumityTemperatureEntry(SQLModel, table = True):
     __tablename__ = "humidity_temperature_entry"
@@ -29,10 +44,12 @@ class HumityTemperatureEntry(SQLModel, table = True):
     temperature: float = Field (ge = -40, le = 70)
     humidity: float = Field (ge = 0, le = 1)
 
-class WetnessEntry(SQLModel, table = True):
-    __tablename__ = "wetness_entry"
-    sensor_id: int = Field(default=0, foreign_key="sensor.serial_number", primary_key=True)
-    sensor: Optional[Sensor] = Relationship(back_populates="wetness_entry")
+class PlantSensorEntry(SQLModel, table = True):
+    __tablename__ = "plant_sensor_entry"
+    sensor_id: int = Field(default=0, foreign_key="plant_sensor.serial_number", primary_key=True)
+    sensor: Optional[PlantSensor] = Relationship(back_populates="plant_sensor_entry")
     entry_timestamp : datetime = Field (primary_key=True)
+    temperature: float = Field (ge = -40, le = 70)
+    humidity: float = Field (ge = 0, le = 1)
     wetness: float = Field (ge = 0, le = 1)
             
