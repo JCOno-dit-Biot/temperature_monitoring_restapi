b98df0b30a2d4330a62f1163f2a4f8e81ee4253f
JCOno-dit-Biot <jc.onoditb@gmail.com>
2024-08-18 15:58:38 -0400

    move parse measurement json to helpers

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,9 +1,27 @@
 from src.models import *
 from src.orm import *
 from typing import Union
+import logging
+from datetime import datetime, timezone
+
+def parse_measurement_dict(data_dict):
+        
+        try:
+            entry_timestamp =  datetime.fromtimestamp(int(data_dict["timestamp"]), tz = timezone.utc)
+        except KeyError as e:
+            entry_timestamp = datetime.now(timezone.utc)
+            data_dict["timestamp"] = entry_timestamp
+            logging.warning(e)
+            logging.warning("could not parse timestamp to utc time, using now as the measurement time")
+
+        measurement_validation = Measurement(**data_dict)
+
+        measurement_obj = create_db_sensor_entry_from_measurement(measurement_validation)
+
+        return measurement_obj
 
 
-def create_db_sensor_entry_from_measurement(measurement: Measurement) -> Union[PlantSensorEntry, HumityTemperatureEntry]:
+def create_db_sensor_entry_from_measurement(measurement: Measurement) -> Union[PlantSensorEntry, HumidityTemperatureEntry]:
 
     db_sensor_entry = None
     #only plant sensors report wetness, the default wetness (if not provided) is None. 
@@ -16,7 +34,7 @@ def create_db_sensor_entry_from_measurement(measurement: Measurement) -> Union[P
             wetness = measurement.wetness
         )
     else:
-        db_sensor_entry = HumityTemperatureEntry(
+        db_sensor_entry = HumidityTemperatureEntry(
             sensor_id = measurement.sensor_id,
             entry_timestamp = measurement.entry_timestamp,
             temperature = measurement.temperature,
